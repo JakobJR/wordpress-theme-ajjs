26a9abde3bff4efa199d712e4f81cb7cd3624f34
JakobJR <jakobjr@hotmail.com>
2020-11-27 23:57:13 +0100

    More work on the single-recipe page

--- wp-content/themes/wordpress-laravel/single-recipe.php
+++ wp-content/themes/wordpress-laravel/single-recipe.php
@@ -2,10 +2,26 @@
 
 <div class="single-page-section">
     <h3>Chicken Masala with with rice and fresh vegetables</h3>
-    <div class="top-img" style="background-image: url('../img/dish-1.jpg');">
-
+    <p>Recipe by Jakob Juel Rasmussen</p>
+    <div class="top-wrapper">
+        <div class="top-img" style="background-image: url(http://localhost/wordpress-theme-ajjs/wp-content/themes/wordpress-laravel/img/dish-1.jpg);"></div>
+        <div class="top-info">
+            <h3>Ingredients</h3>
+            <ul>
+                <li>1 ½ tablespoons olive oil</li>
+                <li>2 teaspoons lemon juice</li>
+                <li>½ teaspoon Dijon mustard</li>
+                <li>¼ teaspoon anchovy paste</li>
+                <li>¼ teaspoon sugar</li>
+                <li>.13 teaspoon kosher salt</li>
+                <li>.13 teaspoon black pepper</li>
+                <li>1 garlic clove, minced</li>
+                <li>½ cup diced seeded tomato</li>
+                <li>3 heads Belgian endive, trimmed</li>
+                <li>1 ounce shaved Parmesan cheese</li>
+            </ul>
+        </div>
     </div>
-
     <div class="section-1">
         <?php get_template_part('includes/section', 'content');?>
     </div>
